0dea3c81307d966826fde4ec8e5c98774eb0ed64
Jorge GC <jorgarcar97@gmail.com>
2024-05-28 16:32:38 +0200

    add header and footer, refactor

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const container = document.querySelector(".container")
 
+const length = 600 / 16;
+
 for (let i = 0; i < 16*16; i++) {
     const div = document.createElement("div");
     div.classList.add("squarediv");
+    div.style.width = `${length}px`;
+    div.style.height = `${length}px`
     container.appendChild(div);
 }
+
+container.addEventListener("mouseover", (event) => {
+    console.log(event.target);
+    event.target.classList.add("painted");
+});
